885a4ee25c4d6dfdbdd9ec5b2868479265a0ece7
hemant.madan <hemant.madan@aryaka.com>
2023-08-17 16:45:49 +0530

    this is the source code to substract two numbers

---
+++ sub.py
@@ -0,0 +1,7 @@
+x = input("Type a number: ")
+y = input("Type another number: ")
+
+result = int(x) - int(y)
+
+print("The sub is: ", result)
+# this is just to test the version 1 of this source code
